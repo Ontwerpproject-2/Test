b63cad285edd6e57578d00baebc3174bb1297b62
stantirions <78724707+stantirions@users.noreply.github.com>
2021-02-08 14:25:09 +0100

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,38 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       test.ino
+    Created:	8/02/2021 10:28:06
+    Author:     LAPTOP-GTQ9A07G\Stan
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+int ledPin = 2;
+
+void setup() {
+	pinMode(ledPin, OUTPUT);
+	Serial.begin(115200);
+}
+
+
+void loop() {
+	Serial.print("Hello");
+	digitalWrite(ledPin, HIGH);
+	delay(100);
+
+	Serial.print("World");
+	digitalWrite(ledPin, LOW);
+	delay(100);
+}
+
